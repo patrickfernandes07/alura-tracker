79b424922e124a0195c6173f03a21db85cc0a675
Patrick Fernandes <pernandes07@gmail.com>
2023-09-17 15:46:22 -0300

    first commit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
-// import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import roteador from './roteador'
 
-createApp(App).mount('#app')
+createApp(App).use(roteador).mount('#app')
